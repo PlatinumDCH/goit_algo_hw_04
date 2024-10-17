b75c854736b10e7399f776bff78dcdee2ca24a14
dima cheban <dimacheban23@gmail.com>
2024-10-17 17:33:53 +0300

    add task_3

---
+++ task_3/main.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from colorama import Fore, init
+
+
+class DirectoryExplorer:
+    def __init__(self, path: Path):
+        self.path = path
+
+    def color_file_name(self, file: Path) -> str:
+        """change color name file"""
+        if file.is_dir():
+            return Fore.BLUE
+        elif file.suffix == ".py":
+            return Fore.YELLOW
+        elif file.suffix == ".ipynb":
+            return Fore.MAGENTA
+        elif file.suffix in [".txt", ".md"]:
+            return Fore.GREEN
+        else:
+            return Fore.RED
+
+    def explore(self, indent: str = ""):
+        if self.path.is_dir():
+            print(f"{indent}{Fore.BLUE}{self.path.name}")
+
+            for item in self.path.iterdir():
+                DirectoryExplorer(item).explore(indent + "    ")
+        else:
+            color = self.color_file_name(self.path)
+            print(f"{indent}{color}{self.path.name}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    init(autoreset=True)
+
+    if len(sys.argv) != 2:
+        print("Not found path argument.")
+        sys.exit(1)
+
+    path = Path(sys.argv[1])
+
+    if path.is_dir() and path.exists():
+        explorer = DirectoryExplorer(path)
+        explorer.explore()
+    else:
+        print("path not found or path not directory.")
